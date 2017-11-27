20fb9b0a373d38086b31e86c1ee725dae6e2bea2
ssc <shangshucheng@hotmail.com>
2017-11-28 05:53:36 +0800

    add ukf test

---
+++ ukf.py
@@ -0,0 +1,43 @@
+from filterpy.kalman import MerweScaledSigmaPoints
+from filterpy.kalman import UnscentedKalmanFilter as UKF
+from filterpy.common import Q_discrete_white_noise
+from numpy.random import randn
+import numpy as np
+
+
+def f_cv(x, dt):
+    """ state transition function for a 
+    constant velocity aircraft"""
+    
+    F = np.array([[1, dt, 0,  0],
+                  [0,  1, 0,  0],
+                  [0,  0, 1, dt],
+                  [0,  0, 0,  1]])
+    return np.dot(F, x)
+
+def h_cv(x):
+    return np.array([x[0], x[2]])
+
+dt=1.0
+
+zs = [np.array([i + randn()*std_x, 
+                i + randn()*std_y]) for i in range(100)] 
+
+
+sigmas = MerweScaledSigmaPoints(4, alpha=.1, beta=2., kappa=1.)
+ukf = UKF(dim_x=4, dim_z=2, fx=f_cv,
+          hx=h_cv, dt=dt, points=sigmas)
+ukf.x = np.array([0., 0., 0., 0.])
+ukf.R = np.diag([0.09, 0.09]) 
+ukf.Q[0:2, 0:2] = Q_discrete_white_noise(2, dt=1, var=0.02)
+ukf.Q[2:4, 2:4] = Q_discrete_white_noise(2, dt=1, var=0.02)
+
+uxs = []
+for z in zs:
+    ukf.predict()
+    ukf.update(z)
+    uxs.append(ukf.x.copy())
+uxs = np.array(uxs)
+
+plt.plot(uxs[:, 0], uxs[:, 2])
+print('UKF standard deviation {:.3f} meters'.format(np.std(uxs - xs)))
