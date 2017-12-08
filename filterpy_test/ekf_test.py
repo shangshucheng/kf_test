14c5f58e3db2f5168a43c06f1c07e04a1706fa06
shangshucheng <shangshucheng@hotmail.com>
2017-12-08 12:23:21 +0800

    add jacobian

--- filterpy_test/ekf_test.py
+++ filterpy_test/ekf_test.py
@@ -2,24 +2,25 @@ from filterpy.kalman import ExtendedKalmanFilter
 import numpy as np
 f = ExtendedKalmanFilter (dim_x=1, dim_z=1)
 z = None
-def hj():
-	j = 1
+def hj(j):
+	j = np.array([[0.]])
 	return j
-def hx():
-	x = 1
+def hx(x):
+	x = np.array([[0.]])
 	return x
 
 
 
 
-f.x = np.array([[2.]])       # initial state (location and velocity)
+f.x = np.array([[-50.]])       # initial state (location and velocity)
 
 f.F = np.array([[1.]])    # state transition matrix
-
+#f.B = np.array([[1.]])
+#f.K = 0.5
 f.H = np.array([[1.]])    # Measurement function
-f.P *= 1000.                 # covariance matrix
-f.R = 5                      # state uncertainty
-f.Q = 0 # process uncertainty
+f.P *= 10.                 # covariance matrix
+f.R = 10                      # state uncertainty
+f.Q = 3 # process uncertainty
 
 
 
@@ -28,7 +29,7 @@ f.Q = 0 # process uncertainty
 while True:
     f.predict()
     i = input('input:')
-    f.update(i)
+    f.update(i, hj, hx)
 
     # do something with the output
     x = f.x
